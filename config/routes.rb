146bd3767b1cc4603e8018a6fed3b5fdbb71e754
Luke-kb <keananbrown@gmail.com>
2020-06-30 14:48:17 +1000

    added links to login/register/generator pages from homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
+  #show home page 
   root "pages#index"
-
+  #show login page
   get "/login", to: "pages#login", as: "login"
-
+  #show register page
   get "/register", to: "pages#register", as: "register"
-
+  #show generator page
   get "/generator", to: "pages#generator", as: "generator"
 
 end
