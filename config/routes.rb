a06118dbabc880c621c1ffb3824c90a945aba118
ben-rochlin <brochlin91@gmail.com>
2020-07-06 16:31:59 +1000

    added users and sounds, havent figued out how to add user_sounds

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 
-  
 
-  #show home page 
-  root "pages#index"
-  #show login page
-  get "/login", to: "pages#login", as: "login"
-  #show register page
-  get "/register", to: "pages#register", as: "register"
-  #show generator page
-  get "/generator", to: "pages#generator", as: "generator"
+
+
+  # #show home page 
+  # root "pages#index"
+  # #show login page
+  # get "/login", to: "pages#login", as: "login"
+  # #show register page
+  # get "/register", to: "pages#register", as: "register"
+  # #show generator page
+  # get "/generator", to: "pages#generator", as: "generator"
   #show user page
   # get "/user", to: "pages#user", as: "user"
   #registration form complete
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_051000) do
+ActiveRecord::Schema.define(version: 2020_07_06_051918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2020_07_06_051000) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_sounds", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "sound_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sound_id"], name: "index_user_sounds_on_sound_id"
+    t.index ["user_id"], name: "index_user_sounds_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "email"
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_07_06_051000) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_sounds", "sounds"
+  add_foreign_key "user_sounds", "users"
 end
