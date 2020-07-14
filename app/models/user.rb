7ea56c50496102219954c225bce64398237c7b98
Luke-kb <keananbrown@gmail.com>
2020-07-14 13:52:37 +1000

    dropped users table and created devise users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
     has_many :user_sounds, dependent: :destroy
     has_many :sounds, through: :user_sounds   
 end
